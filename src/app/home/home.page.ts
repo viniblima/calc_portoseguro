73ca98e3c538fe121fc659d2087081a79c9bf889
Vinicius Branco <viniblima2016@gmail.com>
2021-07-15 10:47:37 -0300

    app completo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  item1 = null;
+  item2 = null;
+  funcao = null;
+  result = null;
+  constructor() { }
 
-  constructor() {}
+  setarValor(valor: any) {
+    this.result = null;
+    if (this.item1 != null) {
+      this.item2 = valor;
+    } else {
+      this.item1 = valor;
+    }
+  }
 
+  limparValor() {
+    this.item1 = null;
+    this.item2 = null;
+    this.funcao = null;
+    this.result = null;
+  }
+
+  setarFuncao(funcaoValor) {
+    if (this.item1 == null) {
+      return;
+    }
+    this.funcao = funcaoValor;
+  }
+
+  calc() {
+    if (this.item1 != null && this.item2 != null) {
+      let calc = {
+        '/': this.item1 / this.item2,
+        '*': this.item1 * this.item2,
+        '-': this.item1 - this.item2,
+        '+': this.item1 + this.item2
+      }
+      this.result = calc[this.funcao];
+      this.item1 = null;
+      this.item2 = null;
+      this.funcao = null;
+    } else {
+      return;
+    }
+  }
 }
